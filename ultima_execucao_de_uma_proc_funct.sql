574940a6ce08b07b2a14afab1fb6fbc12cf07f97
Advaldo Mesquita Moreira Junior <adjuniordba@googlemail.com>
2025-01-03 17:26:18 -0300

    Add files via upload

---
+++ ultima_execucao_de_uma_proc_funct.sql
@@ -0,0 +1,10 @@
+
+SELECT	object_name(m.object_id) AS Nome_Procedure,
+		MAX(qs.last_execution_time) AS Ultima_Execucao
+FROM	sys.sql_modules m
+		LEFT   JOIN (sys.dm_exec_query_stats qs
+               CROSS APPLY sys.dm_exec_sql_text (qs.sql_handle) st ) 
+         ON m.object_id = st.objectid
+        AND st.dbid = db_id()
+GROUP  BY object_name(m.object_id);
+
